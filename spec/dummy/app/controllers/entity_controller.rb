c92a34eed1f001ba91f8d95b3fff0aa146a4180a
Peter Zotov <whitequark@whitequark.org>
2012-04-04 00:20:21 +0400

    Add rspec-rails.

---
+++ spec/dummy/app/controllers/entity_controller.rb
@@ -0,0 +1,23 @@
+class EntityController < ApplicationController
+  include Heimdallr::Resource
+
+  load_and_authorize_resource
+
+  def index
+  end
+
+  def new
+  end
+
+  def create
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+end
