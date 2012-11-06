4de49aa15561a23c2a616238c8fd6bbd80d60467
voidseeker <voidproxy@gmail.com>
2012-11-06 17:30:00 +0600

    Remove #heimdallr_options from Heimdallr::Resource::ClassMethods

--- lib/heimdallr/resource.rb
+++ lib/heimdallr/resource.rb
@@ -5,34 +5,25 @@ module Heimdallr
     extend ActiveSupport::Concern
 
     module ClassMethods
-      def load_resource(options={})
+      def load_resource(options = {})
         options, filter_options = Heimdallr::ResourceImplementation.prepare_options self, options
-        self.own_heimdallr_options = options
 
         before_filter filter_options do |controller|
           Heimdallr::ResourceImplementation.load_resource controller, options
         end
       end
 
-      def load_and_authorize_resource(options={})
+      def load_and_authorize_resource(options = {})
         options, filter_options = Heimdallr::ResourceImplementation.prepare_options self, options
-        self.own_heimdallr_options = options
 
         before_filter filter_options do |controller|
           Heimdallr::ResourceImplementation.load_and_authorize_resource controller, options
         end
-      end
-
-    protected
-
-      def own_heimdallr_options=(options)
-        cattr_accessor :heimdallr_options
-        self.heimdallr_options = options
       end
     end
   end
 
   # {AccessDenied} exception is to be raised when access is denied to an action.
   class AccessDenied < StandardError; end
-  
+
 end
